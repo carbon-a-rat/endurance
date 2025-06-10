f35e662bc488a9aa724fafcdbc8ca83eb3292801
Clément Boittin <clement.boittin@proton.me>
2025-06-10 18:21:38 +0200

    refactor: Remove PocketBaseClient integration from main.cpp

--- horizon/src/main.cpp
+++ horizon/src/main.cpp
@@ -1,4 +1,3 @@
-#include "PocketBaseClient.h"
 #include "i2c_stats.h"
 #include "i2c_utils.h"
 #include "state.h"
@@ -18,9 +17,6 @@
 FlightDataState flightDataState;
 DataRateCounters dataRateCounters = {0, 0, 0, 0, 0};
 
-PocketBaseClient pbClient(DEATH_STAR_POCKETBASE_HOST,
-                          DEATH_STAR_POCKETBASE_PORT);
-
 // --- Data Handling ---
 
 // Reads a command from Serial and sends it to the gateway
@@ -34,16 +30,6 @@ void debugSendCommandToGateway() {
   }
 }
 
-// Callback for PocketBase SSE messages
-void onSSEMessage(const String &data) {
-  Serial.print("[PocketBase SSE] ");
-  Serial.println(data);
-}
-
-// Launcher data JSON initialization
-
-String launcherJSON;
-JsonDocument launcher;
 void setup() {
   Serial.begin(115200);
   initI2C();
@@ -52,38 +38,10 @@ void setup() {
            []() {}  // gotIPCallback
   );
 
-  pbClient.setAuthCollection("launchers");
-
-  pbClient.setAuthCollection("launchers");
-  if (pbClient.login(DEATH_STAR_POCKETBASE_IDENTITY,
-                     DEATH_STAR_POCKETBASE_PASSWORD)) {
-    Serial.println("PocketBase login successful!");
-    launcherJSON = pbClient.getAuthRecordJson();
-    Serial.println("Auth record: " + launcherJSON);
-    // Start realtime connection and set callback
-    pbClient.startRealtime(onSSEMessage);
-    Serial.println("Establishing realtime connection...");
-    // while (!pbClient.isRealtimeConnected()) {
-    //   pbClient.pollRealtime();
-    // }
-    Serial.println("Realtime connection started!");
-
-    deserializeJson(launcher, launcherJSON);
-    String launcherId = launcher["id"].as<String>();
-    pbClient.addRealtimeSubscription("launchers", launcherId);
-    // Subscriptions will be sent to the server automatically when clientId is
-    // received
-  } else {
-    Serial.println("PocketBase login failed!");
-  }
-
   Serial.println("Horizon ready");
 }
 
 void loop() {
-  // Poll for SSE events
-  pbClient.pollRealtime();
-
   static Timer gatewayTimer(DATA_SEND_INTERVAL / 4); // Gateway poll timer
   static Timer padTimer(DATA_SEND_INTERVAL / 2);     // Pad poll timer
   static Timer dataRateTimer(1000);                  // Data rate print timer
@@ -101,8 +59,8 @@ void loop() {
 
   if (dataRateTimer.expired()) {
     printI2CDataRates(dataRateCounters);
+    printFlightData(flightDataState.currentFlightData);
   }
 
   debugSendCommandToGateway();
-  printFlightData(flightDataState.currentFlightData);
 }
